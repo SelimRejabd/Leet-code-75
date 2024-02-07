ae1164ffbba724d1f9360bf504a6f4d35ffc9e9d
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-02-07 12:13:28 +0600

    product of array except self O(n)

--- 238.cpp
+++ 238.cpp
@@ -4,21 +4,24 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans;
+ public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    const int n = nums.size();
+    vector<int> ans(n);
+    vector<int> prefix(n, 1);
+    vector<int> suffix(n, 1);
 
-        for(int i=0; i<nums.size(); i++){
-            int mul=1;
-            for(int j=0; j<nums.size(); j++){
-                if(i==j) continue;
-                mul *= nums[j];
-            }
+    for (int i = 1; i < n; ++i)
+      prefix[i] = prefix[i - 1] * nums[i - 1];
 
-            ans.push_back(mul);
-        }
-        return ans;
-    }
+    for (int i = n - 2; i >= 0; --i)
+      suffix[i] = suffix[i + 1] * nums[i + 1];
+
+    for (int i = 0; i < n; ++i)
+      ans[i] = prefix[i] * suffix[i];
+
+    return ans;
+  }
 };
 
 int main() {
